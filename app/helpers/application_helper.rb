a5d035ca5244a6bbf99a4e8365ff50b609cd4d20
Van Loc <tangvanloc8@gmail.com>
2021-03-30 12:55:25 +0700

    chapter 3,4,5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title
+    base_title = t "sample_app.index.ror_tu_app"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
